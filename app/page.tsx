69511d37573ae57ad819358e3ddfa5bcbac36ae0
Ajitguptadev32 <dev32@bytescrum.com>
2023-11-22 17:52:05 +0530

    d

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,33 @@
+// Home.jsx
+'use client'
 import ListProducts from "@/components/products/ListProducts";
-import Image from "next/image";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import queryString from "query-string";
+interface SearchParams {
+   keyword: string;
+}
+export default function Home({ searchParams }: { searchParams: SearchParams }) {
+   const [products, setProducts] = useState([]);
+   const urlParams = {
+      keyword: searchParams.keyword,
+     
+    };
+    const searchQuery = queryString.stringify(urlParams);
 
-export default function Home() {
-   return <ListProducts />;
+
+   useEffect(() => {
+      // Fetch product data from your API
+      axios
+      .get(`http://localhost:3001/products?${searchQuery}`)
+         .then((response) => {
+            // Check if the response data is an array before setting it in the state
+            setProducts(response.data.products);
+         })
+         .catch((error) => {
+            console.error("Error fetching data:", error);
+         });
+   }, [searchQuery]);
+   
+   return <ListProducts products={products}  />;
 }
--- components/products/ListProducts.jsx
+++ components/products/ListProducts.jsx
@@ -1,27 +1,10 @@
-"use client";
-import StarRatings from "react-star-ratings";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Productitems from "../products/Productitems";
-import Link from "next/link";
-import Filters from "../layouts/Filters";
-import Header from "../layouts/Header";
+// ListProducts.jsx
 
-const ListProducts = ({ product }) => {
-   const [products, setProducts] = useState([]);
-   console.log(products);
-   useEffect(() => {
-      // Fetch product data from your API
-      axios
-         .get("http://localhost:3001/products")
-         .then((response) => {
-            // Check if the response data is an array before setting it in the state
-            setProducts(response.data.products);
-         })
-         .catch((error) => {
-            console.error("Error fetching data:", error);
-         });
-   }, []);
+import React from "react";
+import Productitems from "../products/Productitems";
+import Filters from "../layouts/Filters";
+
+const ListProducts = ({ products }) => {
    return (
       <section className="py-12">
          <div className="container max-w-screen-xl mx-auto px-4">
